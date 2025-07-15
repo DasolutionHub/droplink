15277f9e8aace345397e767ac2b308e1ec26ec9c
Dasolutionhub <dasolutionshub@gmail.com>
2025-07-15 16:43:13 -0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,14 @@
-const CACHE = 'droplink-cache-v1';
-const FILES = [
-  '/',
-  '/index.html',
-  '/icon-180.png',
-  '/icon-512.png'
-];
+const CACHE_NAME = 'droplink-v1';
+const URLS_TO_CACHE = ['/', '/index.html', '/manifest.json', 'icon-180.png', 'icon-512.png'];
 
-// Install
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll(FILES))
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
 });
 
-// Fetch
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request)
-      .then(res => res || fetch(e.request))
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
 });
